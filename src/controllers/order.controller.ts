34ecfac2023f3cac216654b92e450a7b75a78fca
Hansin Susatya <hansinsusatya@gmail.com>
2020-05-16 10:20:20 +0700

    Load Orders

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -25,6 +25,44 @@ export class OrderController {
     });
   }
 
+  @MessagePattern({ cmd: 'orders' })
+  loadOrders(payload: any) {
+    const response =  this.orderService.loadOrders(payload);
+    return response.then(({ data }) => {
+      return {
+        status: HttpStatus.OK,
+        orders: data.orders,
+      };
+    })
+    .catch(err => {
+      throw new RpcException({
+        error: {
+          status: err.response.status,
+          message: err.response.data,
+        },
+      });
+    });
+  }
+
+  @MessagePattern({ cmd: 'order-total' })
+  loadTotalOrderTotalPage(payload: any) {
+    const response =  this.orderService.loadTotalOrderTotalPage(payload);
+    return response.then(({ data }) => {
+      return {
+        status: HttpStatus.OK,
+        total: data.total,
+      };
+    })
+    .catch(err => {
+      throw new RpcException({
+        error: {
+          status: err.response.status,
+          message: err.response.data,
+        },
+      });
+    });
+  }
+
   @MessagePattern({ cmd: 'order-status' })
   loadOrderStatus(payload: any) {
     const response =  this.orderService.loadOrderStatus(payload);
@@ -32,6 +70,25 @@ export class OrderController {
       return {
         status: HttpStatus.OK,
         list: data.list,
+      };
+    })
+    .catch(err => {
+      throw new RpcException({
+        error: {
+          status: err.response.status,
+          message: err.response.data,
+        },
+      });
+    });
+  }
+
+  @MessagePattern({ cmd: 'order-locked' })
+  loadOrderLocked(payload: any) {
+    const response =  this.orderService.loadOrderLocked(payload);
+    return response.then(({ data }) => {
+      return {
+        status: HttpStatus.OK,
+        locked: data.locked,
       };
     })
     .catch(err => {
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -30,4 +30,45 @@ export class OrderService {
     }).toPromise();
   }
 
+  loadTotalOrderTotalPage(payload: any) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${payload.token}`,
+    };
+    return this.httpService.get(process.env.APP_URL + '/api/orders/total',
+      { headers,
+        params: {
+        date: payload.date,
+      },
+    }).toPromise();
+  }
+
+  loadOrders(payload: any) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${payload.token}`,
+    };
+    return this.httpService.get(process.env.APP_URL + '/api/orders',
+      { headers,
+        params: {
+        date: payload.date,
+        shipping: payload.shipping,
+        status: payload.status,
+      },
+    }).toPromise();
+  }
+
+  loadOrderLocked(payload: any) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${payload.token}`,
+    };
+    return this.httpService.get(process.env.APP_URL + '/api/orders/locked',
+      { headers,
+        params: {
+        date: payload.date,
+      },
+    }).toPromise();
+  }
+
 }
