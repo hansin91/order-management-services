377dd90475c9d34c6955e55ed3f9cdabd69dbbe4
hansin91 <hansinsusatya@gmail.com>
2021-06-11 19:17:08 +0700

    Fix Error

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,21 +1,19 @@
 import { Controller, HttpStatus } from '@nestjs/common';
 import { MessagePattern, RpcException } from '@nestjs/microservices';
-import { CategoryService, ShopeeService } from '@services';
+import { CategoryService } from '@services';
 
 @Controller()
 export class CategoryController {
-  constructor(
-    private readonly shopeeService: ShopeeService,
-    private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) {}
 
   @MessagePattern({ cmd: 'add-category' })
   addCategory(payload: any) {
     const response =  this.categoryService.addCategory(payload);
-    return response.then(({ data }) => {
+    return response.then(({ data: {message, newCategory} }) => {
       return {
         status: HttpStatus.OK,
-        message: data.message,
-        category: data.newCategory,
+        message,
+        category: newCategory,
       };
     })
     .catch(err => {
@@ -31,10 +29,10 @@ export class CategoryController {
   @MessagePattern({ cmd: 'save-category' })
   saveCategory(payload: any) {
     const response =  this.categoryService.saveCategory(payload);
-    return response.then(({ data }) => {
+    return response.then(({ data: {categories} }) => {
       return {
         status: HttpStatus.OK,
-        categories: data.categories,
+        categories,
       };
     })
     .catch(err => {
@@ -63,9 +61,42 @@ export class CategoryController {
     });
   }
 
+  @MessagePattern({ cmd: 'load-shopee-categories' })
+  loadShopeeCategories(payload: any) {
+    const response =  this.categoryService.loadShopeeCategories(payload);
+    return response.then(({ data: {categories, level} }) => {
+      return {status: HttpStatus.OK, categories, level};
+    })
+    .catch(err => {
+      throw new RpcException({
+        error: {
+          status: err.response.status,
+          message: err.response.data,
+        },
+      });
+    });
+  }
+
+  @MessagePattern({ cmd: 'load-category-attributes' })
+  loadCategoryAttributes(payload: any) {
+    const response =  this.categoryService.loadCategoryAttributes(payload);
+    return response.then(({ data: {attributes} }) => {
+      return {status: HttpStatus.OK, attributes};
+    })
+    .catch(err => {
+      const {response: {status, data}} = err;
+      throw new RpcException({
+        error: {
+          status,
+          message: data,
+        },
+      });
+    });
+  }
+
   @MessagePattern({ cmd: 'load-shopee-category-attributes' })
   loadShopeeCategoryAttributes(payload: any) {
-    const response =  this.shopeeService.loadShopeeCategoryAttributes(payload);
+    const response =  this.categoryService.loadShopeeCategoryAttributes(payload);
     return response.then(({ data: {attributes} }) => {
       return {status: HttpStatus.OK, attributes};
     })
@@ -83,11 +114,8 @@ export class CategoryController {
   @MessagePattern({ cmd: 'load-category' })
   loadCategory(payload: any) {
     const response =  this.categoryService.loadCategory(payload);
-    return response.then(({ data }) => {
-      return {
-        status: HttpStatus.OK,
-        category: data.category,
-      };
+    return response.then(({ data: {category} }) => {
+      return {status: HttpStatus.OK, category};
     })
     .catch(err => {
       throw new RpcException({
@@ -102,11 +130,8 @@ export class CategoryController {
   @MessagePattern({ cmd: 'delete-category' })
   deleteCategory(payload: any) {
     const response =  this.categoryService.deleteCategory(payload);
-    return response.then(({ data }) => {
-      return {
-        status: HttpStatus.OK,
-        message: data.message,
-      };
+    return response.then(({ data: {message} }) => {
+      return {status: HttpStatus.OK, message};
     })
     .catch(err => {
       throw new RpcException({
--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -23,12 +23,21 @@ export class CategoryService {
   }
 
   loadCategories(payload: any) {
-    const { token, parent, name } = payload;
+    const { token, parent, name, testing } = payload;
     const headers = {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
     };
-    return this.httpService.get(process.env.ADMIN_URL + '/api/categories', { headers, params: { parent, name } }).toPromise();
+    return this.httpService.get(process.env.ADMIN_URL + '/api/categories', { headers, params: { parent, name, testing } }).toPromise();
+  }
+
+  loadShopeeCategories(payload: any) {
+    const { token, testing, shopId } = payload;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    };
+    return this.httpService.get(process.env.ADMIN_URL + '/api/categories/shopee', { headers, params: { shopId, testing } }).toPromise();
   }
 
   loadCategory(payload: any) {
@@ -47,5 +56,24 @@ export class CategoryService {
       'Authorization': `Bearer ${token}`,
     };
     return this.httpService.delete(process.env.ADMIN_URL + '/api/categories/' + id , { headers }).toPromise();
+  }
+
+  loadCategoryAttributes(payload: any) {
+    const {token, id, testing} = payload;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    };
+    return this.httpService.get(process.env.ADMIN_URL + '/api/categories/' + id + '/attributes' , { headers, params: {testing} }).toPromise();
+  }
+
+  loadShopeeCategoryAttributes(payload: any) {
+    const {token, id, testing, shopId} = payload;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    };
+    return this.httpService.get(process.env.ADMIN_URL + '/api/categories/shopee/' + id + '/attributes',
+    {headers, params: {testing, shopId} }).toPromise();
   }
 }
