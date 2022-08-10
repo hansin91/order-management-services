07f24e00f8a2a0e08887f844380809ddb09428c6
hansin91 <hansinsusatya@gmail.com>
2022-08-10 19:51:00 +0700

    queue setting

--- src/queue.ts
+++ src/queue.ts
@@ -1,6 +1,6 @@
-const PORT = Number(process.env.REDIS_PORT);
-const HOST = process.env.REDIS_HOST;
-const PASSWORD = process.env.REDIS_PASSWORD;
+const PORT = Number(process.env.REDIS_PORT)
+const HOST = process.env.REDIS_HOST
+const PASSWORD = process.env.REDIS_PASSWORD
 
 export const Queue = {
   redis: {
@@ -9,9 +9,9 @@ export const Queue = {
     password: PASSWORD 
   },
   settings: {
-    lockDuration: 30000,
+    lockDuration: 50000,
     lockRenewTime: 15000,
-    stalledInterval: 30000, 
+    stalledInterval: 50000, 
     maxStalledCount: 1,
     guardInterval: 5000,
     retryProcessDelay: 5000,
