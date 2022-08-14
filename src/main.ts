8a1b83c4b0e2e9516c8262a11a5f253a902c5093
hansin91 <hansinsusatya@gmail.com>
2022-08-14 22:24:09 +0700

    Edit main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,24 +7,24 @@ import { AppModule } from './app.module'
 const logger = new Logger()
 async function bootstrap() {
   const PORT = Number(process.env.PORT)
-  // const app = await NestFactory.create(AppModule)
-  // app.connectMicroservice({
-  //   transport: Transport.TCP,
-  //   options: {
-  //     host: process.env.HOST,
-  //     port: PORT,
-  //   },
-  // })
-  // await app.startAllMicroservices()
-  // await app.listen(PORT, () => logger.log('TCP Microservice is listening on port '+ PORT))
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.create(AppModule)
+  app.connectMicroservice({
     transport: Transport.TCP,
     options: {
       host: process.env.HOST,
       port: PORT,
     },
   })
-  app.listen(() => logger.log('TCP Microservice is listening on port '+ PORT))
+  await app.startAllMicroservices()
+  await app.listen(3003, () => logger.log('TCP Microservice is listening on port '+ 3003))
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   transport: Transport.TCP,
+  //   options: {
+  //     host: process.env.HOST,
+  //     port: PORT,
+  //   },
+  // })
+  // app.listen(() => logger.log('TCP Microservice is listening on port '+ PORT))
 }
 
 bootstrap()
